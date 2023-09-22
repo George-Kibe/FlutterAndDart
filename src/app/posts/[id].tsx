09ecebf084c1824f0610ca2432b10a9cade0b2b9
George-Kibe <georgekibewambui@gmail.com>
2023-09-22 16:13:24 +0300

    querying a post

--- src/app/posts/[id].tsx
+++ src/app/posts/[id].tsx
@@ -1,18 +1,43 @@
-import { ScrollView, Text } from 'react-native';
+import { ActivityIndicator, ScrollView, Text } from 'react-native';
 import posts from '../../../assets/data/posts.json';
 import PostListItem from '../../components/PostListItem';
 import { useLocalSearchParams, useNavigation } from 'expo-router';
 import { useLayoutEffect, useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+  query MyQuery($id: ID!) {
+    post(id:$id) {
+      id
+      image
+      content
+      profile {
+        image
+        id
+        name
+        position
+      }
+    }
+  }
+`;
 
 export default function PostDetailsScreen() {
   const { id } = useLocalSearchParams();
-
-  const post = posts.find((post) => post.id === id);
+  const {loading, error, data} = useQuery(query, { variables: { id }});
 
   const navigation = useNavigation();
   useLayoutEffect(() => {
     navigation.setOptions({ title: "Post" });
-  }, [post]);
+  }, []);
+
+  if (loading) return <ActivityIndicator />;
+  if (error) {
+    console.log("Error: ", error);
+    return <Text>Something went wrong...</Text>;
+  }
+
+	console.log(data);
+  const post = data.post;
 
   if (!post) {
     return <Text>Post not found</Text>;
